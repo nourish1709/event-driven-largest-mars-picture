147dcfe02a537a5c62c940f4a23ef6dbea3db763
Yevhenii Savonenko <yevs@jacobianengineering.com>
2022-10-14 07:38:36 +0300

    Initial commit

---
+++ src/main/java/com/nourish1709/eventdrivenlargestmarspicture/EventDrivenLargestMarsPictureApplication.java
@@ -0,0 +1,13 @@
+package com.nourish1709.eventdrivenlargestmarspicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenLargestMarsPictureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenLargestMarsPictureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nourish1709/eventdrivenlargestmarspicture/EventDrivenLargestMarsPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nourish1709.eventdrivenlargestmarspicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenLargestMarsPictureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
